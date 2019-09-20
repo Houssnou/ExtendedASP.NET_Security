dd042e65d392fcd67439a407ebd9161a09b495a0
Houssnou <hosni.moubarak@gmail.com>
2019-09-19 21:37:43 -0400

    Files Init

---
+++ ExtendedSecurity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExtendedSecurity.Startup))]
+namespace ExtendedSecurity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
